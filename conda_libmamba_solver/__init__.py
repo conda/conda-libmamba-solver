2a6f9b9b03eb5a9fcb59469c7b2148404efcb2cb
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-01-25 18:05:15 +0100

    public?

--- conda_libmamba_solver/__init__.py
+++ conda_libmamba_solver/__init__.py
@@ -3,7 +3,7 @@ from .libmamba import LibMambaSolver
 from .libmamba2 import LibMambaSolver2
 
 
-def _get_solver_logic(key=None):
+def get_solver_logic(key=None):
     return {
         "libmamba": LibMambaSolver,
         "libmamba2": LibMambaSolver2,
