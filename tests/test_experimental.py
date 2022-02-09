e251764e063a36d3e89efe0ae4aa1530b1fde642
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-02-09 13:53:32 +0100

    add test_cli_flag_and_env_var_settings

--- tests/test_experimental.py
+++ tests/test_experimental.py
@@ -23,7 +23,10 @@ def test_logging():
     "Check we are indeed writing full logs to disk"
     env = os.environ.copy()
     env["CONDA_EXPERIMENTAL_SOLVER"] = "libmamba"
-    stdout = check_output([sys.executable, "-m", "conda", "create", "-y", "-p", _get_temp_prefix(), "--dry-run", "xz"], stderr=STDOUT, universal_newlines=True, env=env)
+    stdout = check_output(
+        [sys.executable, "-m", "conda", "create", "-y", "-p", _get_temp_prefix(), "--dry-run", "xz"],
+        env=env, stderr=STDOUT, universal_newlines=True,
+    )
 
     in_header = False
     for line in stdout.splitlines():
@@ -45,7 +48,7 @@ def test_logging():
         assert "choice rule creation took" in log_contents
 
 
-def test_cli_flag():
+def test_cli_flag_in_help():
     commands_with_flag = (
        ["install"],
        ["update"],
@@ -69,3 +72,31 @@ def test_cli_flag():
     for command in commands_without_flag:
         stdout = check_output([sys.executable, "-m", "conda"] + command + ["--help"], stderr=STDOUT, universal_newlines=True)
         assert "--experimental-solver" not in stdout
+
+
+def test_cli_flag_and_env_var_settings():
+    env_no_var = os.environ.copy()
+    env_libmamba = os.environ.copy()
+    env_classic = os.environ.copy()
+    env_libmamba["CONDA_EXPERIMENTAL_SOLVER"] = "libmamba"
+    env_classic["CONDA_EXPERIMENTAL_SOLVER"] = "classic"
+    command = [sys.executable, "-m", "conda", "create", "-y", "-p", _get_temp_prefix(), "--dry-run", "xz"]
+    cli_libmamba = ["--experimental-solver=libmamba"]
+    cli_classic = ["--experimental-solver=classic"]
+    tests = [
+        {"cmd": command, "env": env_no_var, "solver": "classic"},
+        {"cmd": command, "env": env_classic, "solver": "classic"},
+        {"cmd": command, "env": env_libmamba, "solver": "libmamba"},
+        {"cmd": command + cli_libmamba, "env": env_no_var, "solver": "libmamba"},
+        {"cmd": command + cli_libmamba, "env": env_libmamba, "solver": "libmamba"},
+        {"cmd": command + cli_libmamba, "env": env_classic, "solver": "libmamba"},
+        {"cmd": command + cli_classic, "env": env_no_var, "solver": "classic"},
+        {"cmd": command + cli_classic, "env": env_libmamba, "solver": "classic"},
+        {"cmd": command + cli_classic, "env": env_classic, "solver": "classic"},
+    ]
+    for test in tests:
+        stdout = check_output(test["cmd"], env=test["env"], stderr=STDOUT, universal_newlines=True)
+        if test["solver"] == "libmamba":
+            assert "USING EXPERIMENTAL LIBMAMBA INTEGRATIONS" in stdout
+        else:
+            assert "USING EXPERIMENTAL LIBMAMBA INTEGRATIONS" not in stdout
