e4680894b39466cb8ff03bcc9b9f15a8d4411d92
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-02-15 12:01:29 +0100

    parse channels from lockfile

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -17,16 +17,34 @@ platform = context.subdir
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
+def _get_channels_from_lockfile(path):
+    """Parse `# channels: conda-forge,defaults` comments"""
+    with open(path) as f:
+        for line in f:
+            if line.startswith("# channels:"):
+                return line.split(":")[1].strip().split(",")
+
+
+def _channels_as_args(channels):
+    if not channels:
+        return ()
+    args = ["--override-channels"]
+    for channel in channels:
+        args += ["-c", channel]
+    return tuple(args)
+
+
 @pytest.fixture(scope="module", params=os.listdir(TEST_DATA_DIR))
-def prefix(request):
-    lock_platform = request.param.split(".")[-2]
+def prefix_and_channels(request):
+    lockfile = os.path.join(TEST_DATA_DIR, request.param)
+    lock_platform = lockfile.split(".")[-2]
     if lock_platform != platform:
         pytest.skip(f"Running platform {platform} does not match file platform {lock_platform}")
     with env_var("CONDA_TEST_SAVE_TEMPS", "1"):
         prefix = _get_temp_prefix(use_restricted_unicode=True).replace(" ", "")
-        args = ("--file", os.path.join(TEST_DATA_DIR, request.param))
-        with make_temp_env(*args, prefix=prefix) as prefix:
-            yield prefix
+        with make_temp_env("--file", lockfile, prefix=prefix) as prefix:
+            channels = _get_channels_from_lockfile(lockfile)
+            yield prefix, channels
     shutil.rmtree(prefix)
 
 
@@ -36,24 +54,27 @@ def solver_args(request):
 
 
 @pytest.mark.slow
-def test_a_warmup(prefix, solver_args):
+def test_a_warmup(prefix_and_channels, solver_args):
     """Dummy test to install envs and warm up caches"""
-    prefix, solver_args = prefix, solver_args
+    prefix_and_channels, solver_args = prefix_and_channels, solver_args
 
 
 @pytest.mark.slow
-def test_update_python(prefix, solver_args):
+def test_update_python(prefix_and_channels, solver_args):
+    prefix, channels = prefix_and_channels
     with pytest.raises(DryRunExit):
-        run_command(Commands.UPDATE, prefix, *solver_args, "python")
+        run_command(Commands.UPDATE, prefix, *_channels_as_args(channels), *solver_args, "python")
 
 
 @pytest.mark.slow
-def test_update_python_update_deps(prefix, solver_args):
+def test_update_python_update_deps(prefix_and_channels, solver_args):
+    prefix, channels = prefix_and_channels
     with pytest.raises(DryRunExit):
-        run_command(Commands.INSTALL, prefix, *solver_args, "python", "--update-deps")
+        run_command(Commands.INSTALL, prefix, *_channels_as_args(channels), *solver_args, "python", "--update-deps")
 
 
 @pytest.mark.slow
-def test_update_all(prefix, solver_args):
+def test_update_all(prefix_and_channels, solver_args):
+    prefix, channels = prefix_and_channels
     with pytest.raises(DryRunExit):
-        run_command(Commands.UPDATE, prefix, *solver_args, "--all")
+        run_command(Commands.UPDATE, prefix, *_channels_as_args(channels), *solver_args, "--all")
