f53f832150657e6c9f99a85f46618750b8c7cb7f
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-02-17 14:51:02 +0100

    optimize logging a bit (#11)
    * optimize logging a bit (less verbosity by default, read only if necessary)
    
    * adjust tests

--- conda_libmamba_solver/utils.py
+++ conda_libmamba_solver/utils.py
@@ -1,122 +1,42 @@
 import os
 import sys
-from io import UnsupportedOperation
-from time import sleep
-from threading import Thread
 import traceback
 import tempfile
-
-
-class CapturedDescriptor:
-    """
-    Class used to grab standard output or another stream.
-    """
-
-    def __init__(self, stream=sys.stdout, threaded=False, sentinel="\0"):
-        self._captured_stream = stream
-        self._threaded = threaded
-        self._sentinel = sentinel
-        self.text = ""
-        try:
-            # Keep a reference to the descriptor we are capturing
-            self._captured_stream_fd = self._captured_stream.fileno()
-        except UnsupportedOperation:
-            # This happens in our tests because we are already
-            # capturing sys.stdout and stderr; if that's the case
-            # make this a noop
-            self.start = self._noop
-            self.stop = self._noop
-        else:
-            # Create a pipe so the stream can be captured:
-            self._pipe_out, self._pipe_in = os.pipe()
-
-    def __enter__(self):
-        self.start()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.stop()
-
-    def _noop(self, *args, **kwargs):
-        pass
-
-    def _start(self):
-        """
-        Start capturing the stream data.
-        """
-        self.text = ""
-        # Save a copy of the stream:
-        self._original_stream_fd = os.dup(self._captured_stream_fd)
-        # Replace the original stream with our write pipe:
-        os.dup2(self._pipe_in, self._captured_stream_fd)
-        if self._threaded:
-            # Start thread that will read the stream:
-            self._thread = Thread(target=self._read)
-            self._thread.start()
-            # Make sure that the thread is running and os.read() has executed:
-            sleep(0.01)
-
-    start = _start
-
-    def _stop(self):
-        """
-        Stop capturing the stream data and save the text in `text`.
-        """
-        try:
-            if sys.platform.startswith("win"):
-                os.write(self._pipe_in, self._sentinel.encode())
-                os.fsync(self._pipe_in)
-            else:
-                # Print the escape character to make the _read method stop:
-                self._captured_stream.write(self._sentinel)
-                # Flush the stream to make sure all our data goes in before
-                # the escape character:
-                self._captured_stream.flush()
-            if self._threaded:
-                # wait until the thread finishes so we are sure that
-                # we have the last character:
-                self._thread.join()
-            else:
-                self._read()
-        except Exception as e:
-            traceback.print_exc(file=sys.stdout)
-            self._close()
-            raise
-        else:
-            self._close()
-
-    stop = _stop
-
-    def _close(self):
-        # Close the pipe:
-        os.close(self._pipe_in)
-        os.close(self._pipe_out)
-        # Restore the original stream:
-        os.dup2(self._original_stream_fd, self._captured_stream_fd)
-        # Close the duplicate stream:
-        os.close(self._original_stream_fd)
-
-    def _read(self, length=1):
-        """
-        Read the stream data (one byte at a time)
-        and save the text in `text`.
-        """
-        while True:
-            char = os.read(self._pipe_out, length).decode(self._captured_stream.encoding)
-            if not char or self._sentinel in char:
-                break
-            self.text += char
+from typing import Callable, Optional
 
 
 class CaptureStreamToFile:
-    def __init__(self, stream=sys.stderr, path=None, callback=None):
+    def __init__(
+        self,
+        stream=sys.stderr,
+        path: Optional[str] = None,
+        callback: Optional[Callable] = None,
+        keep: Optional[bool] = False,
+    ):
+        """
+        Capture a stream and forward to a file.
+
+        Parameters
+        ----------
+        stream:
+            Which stream to capture. Usually `sys.stderr`.
+        path:
+            Optional. Path to file in disk. It will be opened with `a+t`. If not
+            provided, a temporary file will be used.
+        callback:
+            A callable that will process the captured text output. It must
+            take a single argument of type `str`.
+        keep:
+            Whether to keep the file contents stored in the `text` attribute.
+        """
         self._original_stream = stream
         if path is None:
-            self._file = tempfile.TemporaryFile(mode='w+t')
+            self._file = tempfile.TemporaryFile(mode="w+t")
         else:
             self._file = open(path, mode="a+t")
         self.text = None
         self._callback = callback
+        self._keep_captured = keep
 
     def start(self):
         self._original_fileno = self._original_stream.fileno()
@@ -126,12 +46,16 @@ class CaptureStreamToFile:
     def stop(self):
         os.dup2(self._saved_original_fileno, self._original_fileno)
         os.close(self._saved_original_fileno)
-        self._file.flush()
-        self._file.seek(0)
-        self.text = self._file.read()
-        self._file.close()
-        if self._callback:
-            self._callback(self.text)
+        try:
+            self._file.flush()
+            self._file.seek(0)
+            if self._keep_captured:
+                self.text = self._file.read()
+            if self._callback:
+                text = self.text if self._keep_captured else self._file.read()
+                self._callback(text)
+        finally:
+            self._file.close()
 
     def __enter__(self):
         try:
