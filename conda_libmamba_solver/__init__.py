f7610f11cb32cc15551b59e4cdc75fa8fa965d33
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-03-09 12:48:10 +0100

    Cut 22.3 (#19)

--- conda_libmamba_solver/__init__.py
+++ conda_libmamba_solver/__init__.py
@@ -7,7 +7,7 @@ We offer two implementations:
   This is the one we will eventually ship as final.
 """
 
-__version__ = "22.2.0"
+__version__ = "22.3.0"
 
 from ._libmamba import LibMambaSolverDraft
 from .solver import LibMambaSolver
