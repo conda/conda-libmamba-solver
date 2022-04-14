44449745bb1f819edaf1fa0b2b6af6878086b754
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-14 19:53:51 +0200

    try to enhance conda build compatibility

--- conda_libmamba_solver/solver.py
+++ conda_libmamba_solver/solver.py
@@ -25,6 +25,7 @@ from conda.common.url import (
     remove_auth,
 )
 from conda.exceptions import (
+    NoPackagesFoundError,  # conda-build compat
     PackagesNotFoundError,
     SpecsConfigurationConflictError,
     UnsatisfiableError,
@@ -601,6 +602,9 @@ class LibMambaSolver(Solver):
             line = line.strip()
             if line.startswith("- nothing provides requested"):
                 packages = line.split()[4:]
+                # TODO: Remove this hack for conda-build compatibility >_<
+                if "conda_build.environ" in sys.modules:
+                    raise NoPackagesFoundError([packages])
                 raise PackagesNotFoundError([" ".join(packages)])
         raise LibMambaUnsatisfiableError(problems)
 
