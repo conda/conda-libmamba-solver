4126f0c83b93589db8ddb6c10532b28863ee8676
jaimergp <jaimergp@users.noreply.github.com>
2023-07-20 12:30:48 +0200

    drop env var just in case

--- tests/test_solver_differences.py
+++ tests/test_solver_differences.py
@@ -232,10 +232,12 @@ def test_too_aggressive_update_to_conda_forge_packages():
     conda classic would just change a few (<5) packages, but libmamba seemed to upgrade
     EVERYTHING it can to conda-forge.
     """
-    with make_temp_env("conda", "python", "--override-channels", "--channel=defaults", "--solver=classic") as prefix:
+    with make_temp_env("conda", "python", "--override-channels", "--channel=defaults") as prefix:
         cmd = "install", "-p", prefix, "-c", "conda-forge", "libzlib", "--json", "--dry-run", "-y", "-vvv"
-        p_classic = conda_subprocess(*cmd, "--solver=classic", explain=True)
-        p_libmamba = conda_subprocess(*cmd, "--solver=libmamba", explain=True)
+        env = os.environ.copy()
+        env.pop("CONDA_SOLVER", None)
+        p_classic = conda_subprocess(*cmd, "--solver=classic", explain=True, env=env)
+        p_libmamba = conda_subprocess(*cmd, "--solver=libmamba", explain=True, env=env)
         data_classic = json.loads(p_classic.stdout)
         data_libmamba = json.loads(p_libmamba.stdout)
         assert len(data_classic["actions"]["LINK"]) < 10 
