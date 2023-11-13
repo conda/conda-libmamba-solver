cf3f2c8833b8cd09e59e42c0696d23d158acf9fd
jaimergp <jaimergp@users.noreply.github.com>
2023-11-13 10:47:44 +0100

    try shorter env path on windows

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 import pytest
 from conda.base.context import reset_context
-from conda.common.compat import on_linux
+from conda.common.compat import on_linux, on_win
 from conda.common.io import env_vars
 from conda.core.prefix_data import PrefixData
 from conda.models.channel import Channel
@@ -237,7 +237,10 @@ def test_conda_build_with_aliased_channels(tmp_path):
     condarc = Path.home() / ".condarc"
     condarc_contents = condarc.read_text() if condarc.is_file() else None
     env = os.environ.copy()
-    env["CONDA_BLD_PATH"] = str(tmp_path / "conda-bld")
+    if on_win:
+        env["CONDA_BLD_PATH"] = str(Path(os.environ.get("RUNNER_TEMP", tmp_path), "bld"))
+    else:
+        env["CONDA_BLD_PATH"] = str(tmp_path / "conda-bld")
     try:
         _setup_conda_forge_as_defaults(Path.home(), force=True)
         conda_subprocess(
