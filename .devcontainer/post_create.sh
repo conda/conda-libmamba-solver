78f392c54db7aeca778485b66a29520211c10872
jaimergp <jaimergp@users.noreply.github.com>
2024-03-18 18:59:52 +0000

    fix mamba support, add some echo's

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,6 +5,8 @@
 # - https://github.com/conda/conda is mounted at /workspaces/conda
 # - https://github.com/conda/conda-libmamba-solver is mounted at
 #   /workspaces/conda-libmamba-solver
+# - https://github.com/mamba-org/mamba is (optionally) mounted at 
+#   /workspaces/mamba
 
 set -euo pipefail
 
@@ -12,9 +14,15 @@ CONDA_SRC=${CONDA_SRC:-/workspaces/conda}
 CONDA_LIBMAMBA_SOLVER_SRC=${CONDA_LIBMAMBA_SOLVER_SRC:-/workspaces/conda-libmamba-solver}
 
 # Clear history to avoid unneeded conflicts
+echo "Clearing base history..."
 echo '' > /opt/conda/conda-meta/history
 
-# Install all dependencies
+if [ ! -f $CONDA_SRC/pyproject.toml ]; then
+    echo "conda/conda not found, cloning..."
+    git clone https://github.com/conda/conda $CONDA_SRC
+fi
+
+echo "Installing dev & test dependencies..."
 /opt/conda/bin/conda install -n base --yes --quiet \
     --file="$CONDA_SRC/tests/requirements.txt" \
     --file="$CONDA_SRC/tests/requirements-ci.txt" \
--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -5,6 +5,8 @@
 # - https://github.com/conda/conda is mounted at /workspaces/conda
 # - https://github.com/conda/conda-libmamba-solver is mounted at
 #   /workspaces/conda-libmamba-solver
+# - https://github.com/mamba-org/mamba is (optionally) mounted at 
+#   /workspaces/mamba
 
 set -euo pipefail
 
@@ -13,43 +15,55 @@ CONDA_SRC=${CONDA_SRC:-/workspaces/conda}
 CONDA_LIBMAMBA_SOLVER_SRC=${CONDA_LIBMAMBA_SOLVER_SRC:-/workspaces/conda-libmamba-solver}
 MAMBA_SRC=${MAMBA_SRC:-/workspaces/mamba}
 
-function recompile-mamba () {
+cat >> ~/.bashrc <<EOF
+function develop-mamba() {
   if [ -f "$MAMBA_SRC/mamba/setup.py" ]; then
+  # We only support mamba 1.x for now; v2 there's no 'mamba' subdirectory
+    # Install mamba dev dependencies only once:
+    if [ ! -f "$MAMBA_SRC/build/installed" ]; then
+      # remove "sel(win)" in environment yaml hack since conda does not understand
+      # libmamba specific specs
+      echo "Installing mamba 1.x in dev mode..."
+      sed '/sel(.*)/d' "$MAMBA_SRC/mamba/environment-dev.yml" > /tmp/mamba-environment-dev.yml
+      "$MINICONDA/condabin/conda" env update -p "$MINICONDA" \
+          --file /tmp/mamba-environment-dev.yml
+      "$MINICONDA/condabin/conda" install make -y  # missing in mamba's dev env
+      "$MINICONDA/condabin/conda" remove -p "$MINICONDA" -y --force libmambapy libmamba
+      # Clean build directory to avoid issues with stale build files
+      test -f "$MAMBA_SRC/build/CMakeCache.txt" && rm -rf "$MAMBA_SRC/build"
+    fi
+    # Compile
     cd "$MAMBA_SRC"
-    sudo "$MINICONDA/bin/cmake" -B build/ \
+    "$MINICONDA/bin/cmake" -B build/ \
         -DBUILD_LIBMAMBA=ON \
         -DBUILD_SHARED=ON \
         -DCMAKE_INSTALL_PREFIX="$MINICONDA" \
         -DCMAKE_PREFIX_PATH="$MINICONDA" \
         -DBUILD_LIBMAMBAPY=ON
-    sudo "$MINICONDA/bin/cmake" --build build/ -j2
-    sudo make install -C build/
+    "$MINICONDA/bin/cmake" --build build/ -j\${NPROC:-2}
+    make install -C build/
     cd -
-  else
-    echo "ERROR: path '$MAMBA_SRC' not mounted so no libmamba to recompile"
+    "$MINICONDA/bin/pip" install -e "$MAMBA_SRC/libmambapy/" --no-deps
+    test -f "$MINICONDA/conda-meta/mamba-"*".json" && "$MINICONDA/bin/pip" install -e "$MAMBA_SRC/mamba/" --no-deps
+    touch "$MAMBA_SRC/build/installed" || true
+  else  
+    echo "Path '$MAMBA_SRC' @ 1.x not mounted so no mamba to develop"
   fi
 }
-
-# We only support mamba 1.x for now; v2 there's no 'mamba' subdirectory
-if [ -f "$MAMBA_SRC/mamba/setup.py" ]; then
-  # remove "sel(win)" in environment yaml hack since conda does not understand
-  # libmamba specific specs
-  sed '/sel(.*)/d' "$MAMBA_SRC/mamba/environment-dev.yml" > /tmp/mamba-environment-dev.yml
-  sudo "$MINICONDA/condabin/conda" env update -p "$MINICONDA" \
-       --file /tmp/mamba-environment-dev.yml
-
-  sudo "$MINICONDA/condabin/conda" remove -p "$MINICONDA" -y --force libmambapy libmamba
-  recompile-mamba
-  sudo "$MINICONDA/bin/pip" install -e "$MAMBA_SRC/libmambapy/" --no-deps
-fi
+EOF
 
 cd "$CONDA_SRC"
+echo "Initializing conda in dev mode..."
 "$MINICONDA/bin/python" -m conda init --dev bash
 cd -
 
+echo "Installing conda-libmamba-solver in dev mode..."
 "$MINICONDA/bin/python" -m pip install -e "$CONDA_LIBMAMBA_SOLVER_SRC" --no-deps
 
 set -x
 conda list -p "$MINICONDA"
 conda info
 conda config --show-sources
+set +x
+test -f "$MAMBA_SRC/mamba/setup.py" \
+  && echo "Mamba mounted at $MAMBA_SRC; source ~/.bashrc and run develop-mamba() for dev-install"
