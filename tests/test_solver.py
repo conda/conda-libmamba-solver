13d47ea60a7dc229681ac7b43fbf80b994d4b5e3
jaimergp <jaimergp@users.noreply.github.com>
2024-03-29 14:42:13 +0000

    These are part of conda/conda, no need to run them here

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -21,40 +21,10 @@ from conda.testing.integration import (
     package_is_installed,
     run_command,
 )
-from conda.testing.solver_helpers import SolverTests
 
 from conda_libmamba_solver.exceptions import LibMambaUnsatisfiableError
-from conda_libmamba_solver.solver2 import LibMambaSolver
 
 from .utils import conda_subprocess
-
-
-class TestLibMambaSolver(SolverTests):
-    @property
-    def solver_class(self):
-        return LibMambaSolver
-
-    @property
-    def tests_to_skip(self):
-        return {
-            "LibMambaSolver does not support track-features/features": [
-                "test_iopro_mkl",
-                "test_iopro_nomkl",
-                "test_mkl",
-                "test_accelerate",
-                "test_scipy_mkl",
-                "test_pseudo_boolean",
-                "test_no_features",
-                "test_surplus_features_1",
-                "test_surplus_features_2",
-                # this one below only fails reliably on windows;
-                # it passes Linux on CI, but not locally?
-                "test_unintentional_feature_downgrade",
-            ],
-            "LibMambaSolver installs numpy with mkl while we were expecting no-mkl numpy": [
-                "test_remove",
-            ],
-        }
 
 
 def test_python_downgrade_reinstalls_noarch_packages():
