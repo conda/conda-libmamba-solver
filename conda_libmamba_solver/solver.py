43aeea372704dc7cb9004c0acab6ac0bb3d2bfcd
jaimergp <jaimergp@users.noreply.github.com>
2024-09-20 19:32:12 +0200

    find out what kind of deduplication is happening

--- conda_libmamba_solver/solver.py
+++ conda_libmamba_solver/solver.py
@@ -199,6 +199,9 @@ class LibMambaSolver(Solver):
         all_channels.extend(in_state.maybe_free_channel())
 
         all_channels = tuple(all_channels)
+        deduped = tuple(dict.fromkeys(all_channels))
+        print(all_channels)
+        print(deduped)
         with Spinner(
             self._spinner_msg_metadata(all_channels, conda_bld_channels=conda_bld_channels),
             enabled=not context.verbosity and not context.quiet,
