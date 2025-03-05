c7c6ee257f92809e2f6808d2013fe5eb0129660b
Ken Odegard <kodegard@anaconda.com>
2025-03-05 13:10:12 -0600

    Adjust getting restore_free_channel from context

--- conda_libmamba_solver/state.py
+++ conda_libmamba_solver/state.py
@@ -392,7 +392,8 @@ class SolverInputState:
                 yield channel
 
     def maybe_free_channel(self) -> Iterable[Channel]:
-        if context.restore_free_channel:
+        # conda 25.3 removed context.restore_free_channel
+        if getattr(context, "restore_free_channel", False):
             yield Channel.from_url("https://repo.anaconda.com/pkgs/free")
 
 
