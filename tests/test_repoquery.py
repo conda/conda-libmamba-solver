774273b7d29ebc0809e43870d2fc5b8041cecb1a
sophia <scastellarin@quansight.com>
2026-05-22 11:26:19 -0700

    xfail repoquery for python packages tests on windows

--- tests/test_repoquery.py
+++ tests/test_repoquery.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import json
 
+import pytest
 from conda.common.compat import on_win
 from conda.models.channel import Channel
 
@@ -51,6 +52,7 @@ def test_query_search():
     )
 
 
+@pytest.mark.xfail(on_win, reason="libmambapy bug https://github.com/mamba-org/mamba/issues/4286")
 def test_query_search_includes_python_site_packages_path():
     index = LibMambaIndexHelper(channels=[Channel("conda-forge")], subdirs=("linux-64", "noarch"))
     results = index.search("python=3.13.2=h4724d56_1_cp313t")
@@ -59,14 +61,6 @@ def test_query_search_includes_python_site_packages_path():
     assert prec.name == "python"
     assert prec.version == "3.13.2"
     if python_site_packages_path_support:
-        # python_site_packages_path is computed by mamba for every python package.
-        # ref: https://github.com/mamba-org/mamba/blob/2.6.2/libmamba/src/core/query.cpp#L237-L245
-        # Windows vs unix and free-threaded vs not free-threaded will have different results.
-        # ref: https://github.com/mamba-org/mamba/pull/4268/changes
-        # issue ref: https://github.com/mamba-org/mamba/issues/4286
-        if on_win:
-            assert prec.python_site_packages_path == "Lib/site-packages"
-        else:
-            assert prec.python_site_packages_path == "lib/python3.13t/site-packages"
+        assert prec.python_site_packages_path == "lib/python3.13t/site-packages"
     else:
         assert prec.python_site_packages_path is None
